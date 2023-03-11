412b3f418f10cbd1acf258949ebbdc0c4a5731fa
bilder <cokoryu@outlook.com>
2023-03-11 03:49:23 +0000

    error: react-hydration-error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,38 @@
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { parseSync } from 'subtitle'
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import videojs from 'video.js';
 
 const inter = Inter({ subsets: [] })
 
 
-function eventStoper(e: React.UIEvent) {
+function stopEvent(e: React.UIEvent) {
   e.stopPropagation()
   e.preventDefault()
 }
 
+// export async function getStaticProps() {
+//   return {
+//     props: {}
+//   }
+// }
+
 export default function Home() {
   const [text, setText] = useState('')
 
-  const handleInputOnChage = (e: React.ChangeEvent<HTMLInputElement>) => {
+  useEffect(() => {
+    const player = videojs.getPlayer('my-player')
+    console.log(player);
+  })
+
+  const handleInputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files
     console.table(files);
   }
 
   const handleDropFile = async (e: React.DragEvent<HTMLElement>) => {
-    eventStoper(e)
+    stopEvent(e)
 
     const file = e.dataTransfer.files[0]
     // It seems that the stream version parser doesn't Web API.
@@ -41,11 +52,31 @@ export default function Home() {
 
   return (
     <>
-      <section onDrop={handleDropFile} onDragOver={eventStoper} onDragEnter={eventStoper} className={styles.drop_box}>
-        <input onChange={handleInputOnChage} type="file" id="input" multiple />
+      <section onDrop={handleDropFile} onDragOver={stopEvent} onDragEnter={stopEvent} className={styles.drop_box}>
+        <input onChange={handleInputOnChange} type="file" id="input" multiple />
       </section>
       <section>
         {text}
+      </section>
+      <section>
+        <video
+          id="my-player"
+          className="video-js"
+          controls
+          preload="auto"
+          poster="//vjs.zencdn.net/v/oceans.png"
+          data-setup='{}'>
+          <source src="//vjs.zencdn.net/v/oceans.mp4" type="video/mp4"></source>
+          <source src="//vjs.zencdn.net/v/oceans.webm" type="video/webm"></source>
+          <source src="//vjs.zencdn.net/v/oceans.ogv" type="video/ogg"></source>
+          <p className="vjs-no-js">
+            To view this video please enable JavaScript, and consider upgrading to a
+            web browser that
+            <a href="https://videojs.com/html5-video-support/" target="_blank">
+              supports HTML5 video
+            </a>
+          </p>
+        </video>
       </section>
     </>
   )
